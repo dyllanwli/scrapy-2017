bce69d8adb01a35b3bf4152d0a36d7db76bbf64e
“dyllanwli” <messsking@gmail.com>
2017-10-22 10:00:55 +0800

    fix bug

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -9,7 +9,7 @@ class QuotesSpider(scrapy.Spider):
         tag = getattr(self,'tag',None)
         if tag is not None:
             url = url + 'tag/' + tag
-            yield scrapy.Request(url.self.parse)
+            yield scrapy.Request(url,self.parse)
 
     def parse(self, response):
         for quote in response.css('div.quote'):
@@ -21,4 +21,4 @@ class QuotesSpider(scrapy.Spider):
         next_page = response.css('li.next a::attr(href)').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(next_page, callback=self.parse) 
